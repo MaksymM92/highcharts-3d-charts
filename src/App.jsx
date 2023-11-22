7eeff294273c941614dcc761dbfc614bcfc920fa
maks <maksym.mostovyi.ua@gmail.com>
2023-11-22 13:27:45 +0400

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BarChart, PieChart } from "./components";
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="3d-charts">
+        <BarChart />
+        <PieChart />
+    </div>
+  )
+}
+
+export default App
